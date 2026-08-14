271adc6ec81de0fa0e5070e61c400125d779aa26
anaviana3-blip <ana.viana3@estudante.ifms.edu.br>
2026-08-14 18:19:55 +0000

    core: mudei para o es6

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,15 +1,15 @@
-const http = require('node:http');
+import http from'node:http';
 
 const porta = 3000
 
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-console.log('servidor funcionando $(req.method) $(req.url)');
+    console.log('servidor funcionando $(req.method) $(req.url)');
 
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain, charset=utf8');
-res.end("servidor funciondo");
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({"chave":"valor"}));
 });
 
 server.listen(porta, () => {
