ab613d9e091a69dbf976158249a5c130d596a008
anaviana3-blip <ana.viana3@estudante.ifms.edu.br>
2026-08-14 18:44:35 +0000

    core: adicionei um console.log que exibe data e hora de cada requisição recebida

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,17 +1,21 @@
-import http from'node:http';
+import http from 'node:http'
 
 const porta = 3000
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    console.log('recurso criado', '$(req.method) $(req.url)');
+const requisicao = (req,res) => {
 
-    res.statusCode = 201;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({"chave":"valor"}));
-});
+    console.log('Recurso criado! ${req.method} ${req.url}');
+    console.log(new Date().toISOString());
 
-server.listen(porta, () => {
-    console.log('servidor ouvindo na porta $(porta)');
-});
+    res.statusCode = 201
+    res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
+    res.end('Data e hora');
+}
+
+server.on('request', requisicao);
+
+server.listen(porta, ()=> {
+    console.log('Servidor ouvindo na porta ${porta}')
+})
